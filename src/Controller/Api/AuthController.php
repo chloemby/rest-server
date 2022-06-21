9e53fdaaed5b25144c157bbfcf3f90231937583d
Vladislav Goncharov <goncharov_vs@drom.ru>
2022-06-21 19:06:48 +0700

    Добавить установку email при регистрации пользователя

--- src/Controller/Api/AuthController.php
+++ src/Controller/Api/AuthController.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use App\Entity\User;
+use App\Exception\AppException;
+use App\Exception\ValidationException;
 use App\Repository\UserRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -32,20 +35,22 @@ class AuthController extends AbstractController
     public function registerAction(Request $request): JsonResponse
     {
         try {
-            $username = $request->get('username');
-            $password = $request->get('password');
+            $username = (string)$request->get('username');
+            $password = (string)$request->get('password');
+            $email = (string)$request->get('email');
 
             $user = new User();
             $user->setUsername($username)
-                ->setPassword($this->userPasswordHasher->hashPassword($user, $password));
+                ->setPassword($this->userPasswordHasher->hashPassword($user, $password))
+                ->setEmail($email);
 
             $this->userRepository->add($user, flush: true);
 
             return new JsonResponse(['username' => $user->getUserIdentifier()]);
-        } catch (\Throwable $e) {
-            return new JsonResponse(
-                $e->getMessage()
-            );
+        } catch (AppException $e) {
+            return new JsonResponse($e->getMessage(), $e->getCode());
+        } catch (\Throwable) {
+            return new JsonResponse('Неизвестная ошибка', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
