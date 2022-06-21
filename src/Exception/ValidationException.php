ca0f121376643ec50dc439569342379c57e61e16
Vladislav Goncharov <goncharov_vs@drom.ru>
2022-06-21 20:45:53 +0700

    Добавление валидации email при регистрации

--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace App\Exception;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class ValidationException extends AppException
 {
-
+    public function __construct(
+        string $message = "",
+        int $code = Response::HTTP_BAD_REQUEST,
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
 }
