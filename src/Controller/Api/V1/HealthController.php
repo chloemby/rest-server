162d2e4396b86b8abca86ae9a045559e43293c4e
Vladislav Goncharov <goncharov_vs@drom.ru>
2022-06-21 19:00:23 +0700

    Обновить ответ /api/v1/health/check

--- src/Controller/Api/V1/HealthController.php
+++ src/Controller/Api/V1/HealthController.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 namespace App\Controller\Api\V1;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route(path: '/api/v1/health')]
 class HealthController extends AbstractController
 {
     #[Route(path: '/check', name: 'api-v1-health-check')]
-    public function checkAction(): JsonResponse
+    public function checkAction(#[CurrentUser] User $user): JsonResponse
     {
-        return new JsonResponse(['health' => 'OK']);
+        return new JsonResponse(
+            [
+                'health' => 'OK',
+                'username' => $user->getUserIdentifier(),
+                'email' => $user->getEmail(),
+            ]
+        );
     }
 }
