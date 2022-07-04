d5e9a054cceca707903aaf07fc4cb76a19851abb
Vladislav Goncharov <goncharov_vs@drom.ru>
2022-07-04 12:22:53 +0700

    Создать сущность Article и репозиторий для нее

---
+++ src/Entity/Article.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JetBrains\PhpStorm\Internal\TentativeType;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article implements \JsonSerializable
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'text')]
+    private string $text;
+
+    #[ORM\Column(name: 'created_at', type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(name: 'created_by', type: 'integer')]
+    private int $createdBy;
+
+    #[ORM\Column(name: 'updated_by', type: 'integer')]
+    private int $updatedBy;
+
+    #[ORM\Column(name: 'updated_at', type: 'datetime_immutable')]
+    private \DateTimeImmutable $updatedAt;
+
+    #[ORM\Column(name: 'deleted_at', type: 'datetime_immutable', nullable: true)]
+    private ?\DateTimeImmutable $deletedAt = null;
+
+    #[ORM\Column(name: 'deleted_by', type: 'integer', nullable: true)]
+    private ?int $deletedBy = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $title;
+
+    public function __construct(User $creator)
+    {
+        $this->createdBy = $this->updatedBy = $creator->getId();
+        $this->createdAt = $this->updatedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): int
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(int $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getUpdatedBy(): int
+    {
+        return $this->updatedBy;
+    }
+
+    public function setUpdatedBy(int $updatedBy): self
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeImmutable
+    {
+        return $this->deletedAt;
+    }
+
+    public function setDeletedAt(\DateTimeImmutable $deletedAt): self
+    {
+        $this->deletedAt = $deletedAt;
+
+        return $this;
+    }
+
+    public function getDeletedBy(): ?int
+    {
+        return $this->deletedBy;
+    }
+
+    public function setDeletedBy(?int $deletedBy): self
+    {
+        $this->deletedBy = $deletedBy;
+
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'title' => $this->getTitle(),
+            'text' => $this->getText(),
+            'createdBy' => $this->getCreatedBy(),
+            'deletedAt' => $this->getDeletedAt()?->getTimestamp(),
+            'deletedBy' => $this->getDeletedBy(),
+            'updatedAt' => $this->getUpdatedAt()?->getTimestamp(),
+            'updatedBy' => $this->getUpdatedBy(),
+        ];
+    }
+}
