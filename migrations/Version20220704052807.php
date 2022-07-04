6efd770d1fc8060e409901a85a4e4f0e35fc7e62
Vladislav Goncharov <goncharov_vs@drom.ru>
2022-07-04 12:47:40 +0700

    Создать миграцию с таблицей связей статьи и категории

---
+++ migrations/Version20220704052807.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220704052807 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            CREATE TABLE articles_categories 
+            (
+                article_id INT NOT NULL,
+                article_category_id INT NOT NULL,
+                INDEX(article_id),
+                INDEX(article_category_id)
+            )
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE articles_categories;
+        SQL);
+    }
+}
